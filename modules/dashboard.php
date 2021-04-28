b0f90660d57d5c987ef0603b2735ace3922f92ab
yeimar <yeimar112003@outlook.com>
2021-04-28 02:52:54 -0500

    !! Todo bien hasta el momento

--- modules/dashboard.php
+++ modules/dashboard.php
@@ -12,7 +12,9 @@ if (!isset($email)) {
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Document</title>
+        <title>Dashboard Principal</title>
+        <!-- Icon -->
+        <link rel="icon" type="image/png" href="../img/icon.png">
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
         <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous" />
     </head>
