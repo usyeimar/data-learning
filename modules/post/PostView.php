9612a9eff3298c7e14c5e214071c52397e76eb6a
yeimar <yeimar112003@outlook.com>
2021-04-30 08:49:50 -0500

    Se creo la plantilla de la pestaña cusos/Ya se musetra la galeria de cursos

---
+++ modules/post/PostView.php
@@ -0,0 +1,92 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <meta name="viewport" content="width=b n, initial-scale=1.0" />
+  <link rel="stylesheet" href="./css/PostStyle.css">
+
+  <title>Document</title>
+</head>
+
+<body>
+  <section class="post-list">
+    <div class="content">
+      <!--Post # 1-->
+      <article class="post">
+        <div class="post-header">
+          <div class="post-img-1"></div>
+        </div>
+        <div class="post-body">
+          <span> 30 de Abril,2021|Dseño Web</span>
+          <h2>Nuevo tutorial de Html</h2>
+          <p class="descripcion">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae
+            id vel accusantium, error vero quia nihil pariatur. Maiores sit
+            tenetur minus minima. Blanditiis, itaque quae. Nam molestias quis
+            facere animi!
+          </p>
+          <a href="#" class="post-link">Leer Mäs..</a>
+        </div>
+      </article>
+      <!--Post #2-->
+      <article class="post">
+        <div class="post-header">
+          <div class="post-img-2"></div>
+        </div>
+        <div class="post-body">
+          <span> 1 de Abril,2021|Php</span>
+          <h2>Desde las bases con php</h2>
+          <p class="descripcion">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae
+            id vel accusantium, error vero quia nihil pariatur. Maiores sit
+            tenetur minus minima. Blanditiis, itaque quae. Nam molestias quis
+            facere animi!
+          </p>
+          <a href="#" class="post-link">Leer Mäs..</a>
+        </div>
+      </article>
+      <!--Post # 3-->
+      <article class="post">
+        <div class="post-header">
+          <div class="post-img-3"></div>
+        </div>
+        <div class="post-body">
+          <span> 5 de Abril,2021|Base de Datos</span>
+          <h2>Mdelado de base de datos</h2>
+          <p class="descripcion">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae
+            id vel accusantium, error vero quia nihil pariatur. Maiores sit
+            tenetur minus minima. Blanditiis, itaque quae. Nam molestias quis
+            facere animi!
+          </p>
+          <a href="#" class="post-link">Leer Mäs..</a>
+        </div>
+      </article>
+
+      <!--Post # 4-->
+      <article class="post">
+        <div class="post-header">
+          <div class="post-img-4"></div>
+        </div>
+        <div class="post-body">
+          <span> 5 de Abril,2021|Programa tu propia nube</span>
+          <h2>Confihurar una Nube con Nextcloud</h2>
+          <p class="descripcion">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae
+            id vel accusantium, error vero quia nihil pariatur. Maiores sit
+            tenetur minus minima. Blanditiis, itaque quae. Nam molestias quis
+            facere animi!
+          </p>
+          <a href="#" class="post-link">Leer Mäs..</a>
+        </div>
+      </article>
+    </div>
+  </section>
+  <script src="./js/main.js">
+
+  </script>
+</body>
+
+</html>
